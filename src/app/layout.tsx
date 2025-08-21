2c507aa71c181813b8aaaf2aa129c66a5a5d6494
troy-vallarta <vallarta.troy@gmail.com>
2025-08-21 22:54:04 +0800

    added openGraph image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,11 @@ export const metadata: Metadata = {
   title: "Stay Focused",
   description: "Take back your focus and stay lock in",
   icons: [{ rel: "icon", url: "/focus-dashboard.ico" }],
+  openGraph: {
+    images: [
+      "https://og-image-wheat.vercel.app/api/og?title=Stay+Focused&description=Take+back+your+focus+and+stay+lock+in&logoUrl=https%3A%2F%2Fraw.githubusercontent.com%2FZofserif%2Ffocus-dashboard%2Frefs%2Fheads%2Fmain%2Fpublic%2Fstay-focused-icon.png",
+    ],
+  },
 };
 
 const geist = Geist({
